481b3ec03eb53c0a26726d880f24da619a5d2d68
Eric Li <15696082812@163.com>
2025-11-09 00:38:16 +0100

    cctv save

--- player/side_screen.gd
+++ player/side_screen.gd
@@ -3,3 +3,6 @@ extends SubViewportContainer
 @onready var button_1: Button = %Button1
 @onready var button_2: Button = %Button2
 @onready var button_3: Button = %Button3
+
+func _on_button_1_mouse_entered() -> void:
+	print("A")
